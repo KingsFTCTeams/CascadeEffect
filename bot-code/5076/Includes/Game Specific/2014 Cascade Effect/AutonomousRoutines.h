83f73db6cb06b27d48f8b47b01219f6f626adaf4
PaytonKingsFRC <pratzliff@gmail.com>
2014-12-18 14:22:56 -0800

    Updates
    Updated servo names, values, encoder values, etc.

--- bot-code/5076/Includes/Game Specific/2014 Cascade Effect/AutonomousRoutines.h
+++ bot-code/5076/Includes/Game Specific/2014 Cascade Effect/AutonomousRoutines.h
@@ -2,15 +2,19 @@ void DriveOffRamp()
 {
 	nMotorEncoder[Lift] = 0;
 	DriveStraight(3.0, 75);
-	while(nMotorEncoder[Lift] < 12800) {
+	while(nMotorEncoder[Lift] < 13000) {
 		motor[Lift] = 100;
 	}
 	motor[Lift] = 0;
-	DriveStraight(0.6, 30);
-	wait1Msec(1000);
+	servo[PlatformServo] = 198;
+	wait1Msec(2250);
+	servo[PlatformServo] = 127;
 	motor[Collector] = -100;
 	wait1Msec(1500);
 	motor[Collector] = 0;
+	servo[PlatformServo] = 64;
+	wait1Msec(2250);
+	servo[PlatformServo] = 127;
 	DriveStraight(-0.2, 30);
 	while(nMotorEncoder[Lift] > 0) {
 		motor[Lift] = -100;
